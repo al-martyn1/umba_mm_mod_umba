45d824552cf4777c268a18e1bb5937966c72a03d
al-martyn1 <amart@mail.ru>
2023-05-07 17:59:55 +0300

    app_helpers переехали в cli_tool_helpers

--- cli_tool_helpers.h
+++ cli_tool_helpers.h
@@ -26,10 +26,70 @@
 
 
 
+
+//----------------------------------------------------------------------------
+
+// requires vars deckared/defined before
+//   appVersion defined before
+//   appFullName defined before
+//   appCommitHash std::string compatible defined before
+//   appBuildDate and appBuildTime
+
+extern std::string appFullName  ;
+extern std::string appVersion   ;
+extern std::string appCommitHash;  //UNDONE
+extern std::string appBuildDate ;
+extern std::string appBuildTime ;
+
+
+
+
 //----------------------------------------------------------------------------
 // umba::cli_tool_helpers::
 namespace umba {
 namespace cli_tool_helpers {
+
+
+
+//----------------------------------------------------------------------------
+// https://stackoverflow.com/questions/11697820/how-to-use-date-and-time-predefined-macros-in-as-two-integers-then-stri
+
+template<typename StreamType> inline
+StreamType& printOnlyVersion(StreamType &os)
+{
+    os << appVersion << "\n";
+    return os;
+}
+
+template<typename StreamType> inline
+StreamType& printNameVersion( StreamType &os, const std::string &indent = "" )
+{
+    os << indent << appFullName << " version ";
+    printOnlyVersion(os);
+    return os;
+}
+
+template<typename StreamType> inline
+StreamType& printCommitHash( StreamType &os, const std::string &indent = "" )
+{
+    if (appCommitHash.empty())
+        return os;
+
+    os << indent << "#" << appCommitHash << "\n";
+
+    return os;
+}
+
+template<typename StreamType> inline
+StreamType& printBuildDateTime( StreamType &os, const std::string &indent = "" )
+{
+    os << indent << "Built at " << appBuildDate << " "<< appBuildTime << "\n";
+    return os;
+}
+
+//----------------------------------------------------------------------------
+
+
 
 
 
