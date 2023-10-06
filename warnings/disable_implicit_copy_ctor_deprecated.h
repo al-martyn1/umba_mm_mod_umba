24a67152e7164d20e07d74927a47798119b33139
al-martyn1 <amart@mail.ru>
2023-10-06 08:16:05 +0300

    Добавил запрет варнинга C5267: definition of implicit copy constructor for 'i_base' is deprecated because it has a user-provided destructor

---
+++ warnings/disable_implicit_copy_ctor_deprecated.h
@@ -0,0 +1,4 @@
+#if defined(_MSC_VER) && _MSC_VER>=1930
+    // MSVC 2022+
+    #pragma warning(disable:5267) // warning C5267: definition of implicit copy constructor for 'i_base' is deprecated because it has a user-provided destructor
+#endif
---
+++ warnings/push_disable_implicit_copy_ctor_deprecated.h
@@ -0,0 +1,2 @@
+#include "push.h"
+#include "disable_implicit_copy_ctor_deprecated.h"
