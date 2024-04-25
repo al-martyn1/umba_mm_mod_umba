81a6ceb6a298c33fcc7b408dc7837842b060a481
al-martyn1 <amart@mail.ru>
2024-04-26 01:25:55 +0300

    Вынес генератор ID в отдельный от транслитерации хидер

---
+++ id_gen.h
@@ -0,0 +1,133 @@
+#pragma once
+
+#include "transliteration.h"
+
+namespace umba {
+
+
+//----------------------------------------------------------------------------
+
+
+
+
+//----------------------------------------------------------------------------
+inline
+std::string generateIdFromText_generic(const std::string &t, char replaceChar)
+{
+    std::string transliterated = transliterate(t);
+
+    std::string resText; resText.reserve(transliterated.size());
+
+    for(char ch: transliterated)
+    {
+        if (ch>='a' && ch<='z')
+        {
+            resText.append(1u, ch);
+        }
+        else if (ch>='A' && ch<='z')
+        {
+            resText.append(1u, ch-'A'+'a');
+        }
+        else if (ch>='0' && ch<='9')
+        {
+            resText.append(1u, ch);
+        }
+        else if (ch==' ')
+        {
+            resText.append(1u, '-');
+        }
+        else
+        {
+            if (replaceChar!=0)
+            {
+                resText.append(1u, replaceChar); // иначе что-то незнакомое просто пропускаем, без замены
+            }
+        }
+    }
+
+    return resText;
+}
+
+//----------------------------------------------------------------------------
+inline
+std::string generateIdFromText_forDoxygen(const std::string &t, char replaceChar = '-')
+{
+    return generateIdFromText_generic(t, '-');
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+/*
+    GitHub (ПшеРги) правила генерации идентификаторов
+
+    1) Добавляется префикс "user-content-"
+    2) Национальные символы переводятся в нижний регистр
+    3) Пробел заменяется на минус ('-')
+    4) Гитхаб просто пропускает некоторые символы:
+       !, ", #, $
+       %, &, ' (apos)
+       (, ), *, +
+       , (запятая)
+       минус - это минус, добавляет
+       . (точка)
+       :, ;, <, =, >, ?
+       @, [, \, ], ^
+       _ (подчеркивание) - добавляет
+       ` (бэктик)
+       {|}
+       ~
+
+ */
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+inline
+std::unordered_map<std::string, std::string> makeGitHubIdTranslation()
+{
+    std::unordered_map<std::string, std::string> m;
+
+    m["-"] = "-";
+    m[" "] = "-";
+    m["_"] = "_";
+
+    return m;
+}
+
+UMBA_TRANSLITERTION_IMPLEMENT_GET_TRANSLIT_MAP(GitHubIdTranslation)
+//----------------------------------------------------------------------------
+
+
+
+
+
+// inline
+// std::string transliterateEx( const std::string &str, const std::unordered_map<std::string, std::string> &m )
+
+
+
+
+
+
+
+
+// inline
+// std::string generateIdFromTextGeneric(const std::string &t, char replaceChar = '-')
+// {
+//     std::string transliterated = transliterate(t);
+//----------------------------------------------------------------------------
+
+
+
+
+
+} // namespace umba
+
+
+
+
+
--- transliteration.h
+++ transliteration.h
@@ -6,6 +6,16 @@
 #include <unordered_map>
 
 
+#define UMBA_TRANSLITERTION_IMPLEMENT_GET_TRANSLIT_MAP(whichMap)                            \
+                                                                                            \
+            inline                                                                          \
+            const std::unordered_map<std::string, std::string>& get##whichMap()             \
+            {                                                                               \
+                static std::unordered_map<std::string, std::string> m = make##whichMap();   \
+                return m;                                                                   \
+            }
+
+
 namespace umba {
 
 //----------------------------------------------------------------------------
@@ -14,78 +24,63 @@ namespace umba {
 
 //----------------------------------------------------------------------------
 inline
-std::unordered_map<std::string, std::string> makeTransliterationMapUtf8()
+void mergeTransliterationMaps(std::unordered_map<std::string, std::string> &mergeTo, const std::unordered_map<std::string, std::string> &mergeFrom)
+{
+    std::unordered_map<std::string, std::string>::const_iterator it = mergeFrom.begin();
+    for(; it!=mergeFrom.end(); ++it)
+    {
+        mergeTo[it->first] = it->second;
+    }
+}
+
+//----------------------------------------------------------------------------
+inline
+std::unordered_map<std::string, std::string> mergeTransliterationMaps(const std::vector< std::unordered_map<std::string, std::string> > &maps)
+{
+    std::unordered_map<std::string, std::string> resMap;
+
+    if (maps.empty())
+        return resMap;
+
+    std::vector< std::unordered_map<std::string, std::string> >::const_iterator it = maps.begin();
+    resMap = *it;
+
+    for(++it; it!=maps.end(); ++it)
+    {
+        mergeTransliterationMaps(resMap, *it);
+    }
+
+    return resMap;
+}
+
+//----------------------------------------------------------------------------
+//! Транслитерация руссского абы как, из вики, что показалось более привычным, не по каким-то стандартам
+inline
+std::unordered_map<std::string, std::string> makeTransliterationMapUtf8_ru()
 {
     std::unordered_map<std::string, std::string> m;
 
     // Транслитерация русского алфавита латиницей - https://ru.wikipedia.org/wiki/%D0%A2%D1%80%D0%B0%D0%BD%D1%81%D0%BB%D0%B8%D1%82%D0%B5%D1%80%D0%B0%D1%86%D0%B8%D1%8F_%D1%80%D1%83%D1%81%D1%81%D0%BA%D0%BE%D0%B3%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D0%B0_%D0%BB%D0%B0%D1%82%D0%B8%D0%BD%D0%B8%D1%86%D0%B5%D0%B9
 
-    m["А"] = "A";
-    m["а"] = "a";
-    m["Б"] = "B";
-    m["б"] = "b";
-    m["В"] = "V";
-    m["в"] = "v";
-    m["Г"] = "G";
-    m["г"] = "g";
-    m["Д"] = "D";
-    m["д"] = "d";
-    m["Е"] = "E";
-    m["е"] = "e";
-    m["Ё"] = "Yo";
-    m["ё"] = "yo";
-    m["Ж"] = "Zh";
-    m["ж"] = "zh";
-    m["З"] = "Z";
-    m["з"] = "z";
-    m["И"] = "I";
-    m["и"] = "i";
-    m["Й"] = "Y";
-    m["й"] = "y";
-    m["К"] = "K";
-    m["к"] = "k";
-    m["Л"] = "L";
-    m["л"] = "l";
-    m["М"] = "M";
-    m["м"] = "m";
-    m["Н"] = "N";
-    m["н"] = "n";
-    m["О"] = "O";
-    m["о"] = "o";
-    m["П"] = "P";
-    m["п"] = "p";
-    m["Р"] = "R";
-    m["р"] = "r";
-    m["С"] = "S";
-    m["с"] = "s";
-    m["Т"] = "T";
-    m["т"] = "t";
-    m["У"] = "U";
-    m["у"] = "u";
-    m["Ф"] = "F";
-    m["ф"] = "f";
-    m["Х"] = "Kh";
-    m["х"] = "kh";
-    m["Ц"] = "Ts";
-    m["ц"] = "ts";
-    m["Ч"] = "Ch";
-    m["ч"] = "ch";
-    m["Ш"] = "Sh";
-    m["ш"] = "sh";
-    m["Щ"] = "Shch";
-    m["щ"] = "shch";
-    m["Ъ"] = "\"";
-    m["ъ"] = "\"";
-    m["Ы"] = "Y";
-    m["ы"] = "Y";
-    m["Ь"] = "'";
-    m["ь"] = "'";
-    m["Э"] = "E";
-    m["э"] = "e";
-    m["Ю"] = "Yu";
-    m["ю"] = "yu";
-    m["Я"] = "Ya";
-    m["я"] = "ya";
+    m["А"] = "A";    m["а"] = "a";        m["З"] = "Z";    m["з"] = "z";
+    m["Б"] = "B";    m["б"] = "b";        m["И"] = "I";    m["и"] = "i";
+    m["В"] = "V";    m["в"] = "v";        m["Й"] = "Y";    m["й"] = "y";
+    m["Г"] = "G";    m["г"] = "g";        m["К"] = "K";    m["к"] = "k";
+    m["Д"] = "D";    m["д"] = "d";        m["Л"] = "L";    m["л"] = "l";
+    m["Е"] = "E";    m["е"] = "e";        m["М"] = "M";    m["м"] = "m";
+    m["Ё"] = "Yo";   m["ё"] = "yo";       m["Н"] = "N";    m["н"] = "n";
+    m["Ж"] = "Zh";   m["ж"] = "zh";       m["О"] = "O";    m["о"] = "o";
+
+    m["П"] = "P";    m["п"] = "p";        m["Ч"] = "Ch";   m["ч"] = "ch";  
+    m["Р"] = "R";    m["р"] = "r";        m["Ш"] = "Sh";   m["ш"] = "sh";  
+    m["С"] = "S";    m["с"] = "s";        m["Щ"] = "Shch"; m["щ"] = "shch";
+    m["Т"] = "T";    m["т"] = "t";        m["Ъ"] = "\"";   m["ъ"] = "\"";  
+    m["У"] = "U";    m["у"] = "u";        m["Ы"] = "Y";    m["ы"] = "Y";   
+    m["Ф"] = "F";    m["ф"] = "f";        m["Ь"] = "'";    m["ь"] = "'";   
+    m["Х"] = "Kh";   m["х"] = "kh";       m["Э"] = "E";    m["э"] = "e";   
+    m["Ц"] = "Ts";   m["ц"] = "ts";       m["Ю"] = "Yu";   m["ю"] = "yu";  
+
+    m["Я"] = "Ya";   m["я"] = "ya";
 
     // m[""] = "";
 
@@ -93,18 +88,36 @@ std::unordered_map<std::string, std::string> makeTransliterationMapUtf8()
 }
 
 //----------------------------------------------------------------------------
+//! Для генерации ID для гитхаба
 inline
-const std::unordered_map<std::string, std::string>& getTransliterationMapUtf8()
+std::unordered_map<std::string, std::string> makeLowercaseMapUtf8_ru()
 {
-    static std::unordered_map<std::string, std::string> m = makeTransliterationMapUtf8();
+    std::unordered_map<std::string, std::string> m;
+
+    m["А"] = "а";    m["З"] = "з";    m["П"] = "п";    m["Ч"] = "ч";
+    m["Б"] = "б";    m["И"] = "и";    m["Р"] = "р";    m["Ш"] = "ш";
+    m["В"] = "в";    m["Й"] = "й";    m["С"] = "с";    m["Щ"] = "щ";
+    m["Г"] = "г";    m["К"] = "к";    m["Т"] = "т";    m["Ъ"] = "ъ";
+    m["Д"] = "д";    m["Л"] = "л";    m["У"] = "у";    m["Ы"] = "ы";
+    m["Е"] = "е";    m["М"] = "м";    m["Ф"] = "ф";    m["Ь"] = "ь";
+    m["Ё"] = "ё";    m["Н"] = "н";    m["Х"] = "х";    m["Э"] = "э";
+    m["Ж"] = "ж";    m["О"] = "о";    m["Ц"] = "ц";    m["Ю"] = "ю";    m["Я"] = "я";
+
     return m;
 }
 
 //----------------------------------------------------------------------------
+UMBA_TRANSLITERTION_IMPLEMENT_GET_TRANSLIT_MAP(TransliterationMapUtf8_ru)
+UMBA_TRANSLITERTION_IMPLEMENT_GET_TRANSLIT_MAP(LowercaseMapUtf8_ru)
+
+//----------------------------------------------------------------------------
 inline
-std::string transliterate( const std::string &str )
+std::string transliterateEx( const std::string &str
+                           , const std::unordered_map<std::string, std::string> &m
+                           , char replaceChar = 0
+                           )
 {
-    const std::unordered_map<std::string, std::string> &m = getTransliterationMapUtf8();
+    //const std::unordered_map<std::string, std::string> &m = getTransliterationMapUtf8();
 
     std::string resStr; resStr.reserve(str.size()/2u);
 
@@ -123,7 +136,10 @@ std::string transliterate( const std::string &str )
         std::unordered_map<std::string, std::string>::const_iterator it = m.find(curUtfSymbol);
         if (it==m.end())
         {
-            resStr.append(curUtfSymbol);
+            if (replaceChar!=0) // либо заменяем, либо пропускаем
+            {
+                resStr.append(curUtfSymbol);
+            }
         }
         else
         {
@@ -138,33 +154,22 @@ std::string transliterate( const std::string &str )
 
 //----------------------------------------------------------------------------
 inline
-std::string generateIdFromText(const std::string &t, char replaceChar = '-')
+std::string transliterateEx( const std::string &str
+                           , const std::vector< std::unordered_map<std::string, std::string> > &mv
+                           , char replaceChar = 0
+                           )
 {
-    std::string transliterated = transliterate(t);
+    return transliterateEx(str, mergeTransliterationMaps(mv), replaceChar );
+}
 
-    std::string resText; resText.reserve(transliterated.size());
-
-    for(char ch: transliterated)
-    {
-        if (ch>='a' && ch<='z')
-        {
-            resText.append(1u, ch);
-        }
-        else if (ch>='A' && ch<='z')
-        {
-            resText.append(1u, ch-'A'+'a');
-        }
-        else if (ch>='0' && ch<='9')
-        {
-            resText.append(1u, ch);
-        }
-        else
-        {
-            resText.append(1u, replaceChar);
-        }
-    }
-
-    return resText;
+//----------------------------------------------------------------------------
+inline
+std::string transliterate( const std::string &str )
+{
+    return transliterateEx( str
+                          , std::vector< std::unordered_map<std::string, std::string> >{ getTransliterationMapUtf8_ru()
+                                                                                       }
+                          );
 }
 
 //----------------------------------------------------------------------------
