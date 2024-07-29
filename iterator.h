e9723dc3a1320f6b8afb7b5f6ebe55748e91a4a1
Alexandr Martynov <mag@unicont.com>
2024-07-29 21:26:18 +0300

    Фиксанул пробельчики

--- iterator.h
+++ iterator.h
@@ -11,23 +11,23 @@ namespace iterator {
 // struct SimpleInputIterator - https://en.cppreference.com/w/cpp/iterator/input_iterator
 
 // Example
-//  
+//
 // A minimum input iterator.
-//  
+//
 // #include <cstddef>
 // #include <iterator>
-//  
+//
 // struct SimpleInputIterator
 // {
 //     using difference_type = std::ptrdiff_t;
 //     using value_type = int;
-//  
+//
 //     int operator*() const;
-//  
+//
 //     SimpleInputIterator& operator++();
 //     void operator++(int) { ++*this; }
 // };
-//  
+//
 // static_assert(std::input_iterator<SimpleInputIterator>);
 
 
