9dc01f999982afe575dab8d14352c0bd2f5ad08f
al-martyn1 <amart@mail.ru>
2024-07-29 10:44:13 +0300

    Пилим токенизер

--- tokeniser.h
+++ tokeniser.h
@@ -12,30 +12,189 @@
     #endif
 #endif
 
-
+//----------------------------------------------------------------------------
 #include "c_tokeniser.h"
 //
 #include "char_class.h"
 
+//
+#include <map>
+#include <deque>
+#include <iostream>
+
+//
+#include "umba/assert.h"
+
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
 
 // Наткнулся тут
 // #include "microcode_tokenizer.h"
 // В каком-то проекте же делал, надо поискать, что там
 
+//----------------------------------------------------------------------------
 
-//
+
+
+//----------------------------------------------------------------------------
+
+// umba::tokeniser::
 namespace umba {
 namespace tokeniser {
 
 
 
-using trie_index_type               = UMBA_TOKENISER_TRIE_INDEX_TYPE;
-inline constexpr trie_index_invalid = UMBA_TOKENISER_TRIE_INDEX_INVALID;
+//----------------------------------------------------------------------------
+using  trie_index_type                                     = UMBA_TOKENISER_TRIE_INDEX_TYPE;
+inline constexpr const trie_index_type trie_index_invalid  = UMBA_TOKENISER_TRIE_INDEX_INVALID;
+inline constexpr const trie_index_type trie_index_initial  = UMBA_TOKENISER_TRIE_INDEX_INITIAL;
 
-using token_id_type                 = UMBA_TOKENISER_TOKEN_ID_TYPE;
-inline constexpr token_id_invalid   = UMBA_TOKENISER_TOKEN_ID_INVALID;
+//----------------------------------------------------------------------------
+using  token_id_type                                       = UMBA_TOKENISER_TOKEN_ID_TYPE;
+inline constexpr const token_id_type token_id_invalid      = UMBA_TOKENISER_TOKEN_ID_INVALID;
+
+//----------------------------------------------------------------------------
+using TrieNode = umba_tokeniser_trie_node;
+
+//----------------------------------------------------------------------------
 
 
+
+//----------------------------------------------------------------------------
+inline
+void trieNodeInitMakeUninitialized(TrieNode &node)
+{
+    umba_tokeniser_trie_node_init_make_uninitialized(&node);
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+template<typename ContainerType>
+trie_index_type tokenTrieFindNext(const ContainerType &tokenTrie, trie_index_type curIndex, char ch)
+{
+    // Если на входе инвалид, то начинать надо со старта
+
+    trie_index_type lookupChunkStartIdx  = 0;
+    trie_index_type lookupChunkSize      = 0;
+
+    if (curIndex==umba::tokeniser::trie_index_invalid)
+    {
+        lookupChunkStartIdx  = tokenTrie[0].lookupChunkStartIndex;
+        lookupChunkSize      = tokenTrie[0].lookupChunkSize;      
+    }
+    else
+    {
+        if (curIndex>=tokenTrie.size())
+        {
+            return trie_index_invalid;
+        }
+
+        lookupChunkStartIdx = tokenTrie[curIndex].childsIndex;
+        UMBA_ASSERT(lookupChunkStartIdx<tokenTrie.size());
+        // if (levelStartIdx>=tokenTrie.size()) // тут бы assert, тут не штатная ситуация
+        // {
+        //     return umba::tokeniser::trie_index_invalid;
+        // }
+
+        lookupChunkSize = tokenTrie[levelStartIdx].lookupChunkSize;
+    }
+
+    for(trie_index_type i=0; i!=lookupChunkSize; ++i)
+    {
+        const auto idx = lookupChunkStartIdx + i;
+        UMBA_ASSERT(idx<tokenTrie.size());
+        // if (idx>=tokenTrie.size()) // тут бы assert, тут не штатная ситуация
+        // {
+        //     return umba::tokeniser::trie_index_invalid;
+        // }
+
+        if (tokenTrie[idx].symbol==ch)
+            return idx;
+
+        if (tokenTrie[idx].symbol>ch)
+            return trie_index_invalid; // у нас символы отсортированы по возрастанию, и если код искомого символа больше того, что мы обнаружили в очередной entry, то дальше искать нет смысла
+    }
+
+    return trie_index_invalid; // Не нашли, обломс
+}
+
+//----------------------------------------------------------------------------
+
+
+
+//----------------------------------------------------------------------------
+class TrieBuilder
+{
+
+protected:
+
+    struct TrieBuilderMapNode
+    {
+        umba::tokeniser::TrieNode             trieNode;
+        std::map<char, TrieBuilderMapNode>    childs  ;
+    
+        TrieBuilderMapNode() : trieNode(), childs()
+        {
+            trieNodeInitMakeUninitialized(trieNode);
+        }
+    
+        TrieBuilderMapNode(const TrieBuilderMapNode &) = default;
+        TrieBuilderMapNode& operator=(const TrieBuilderMapNode &) = default;
+        TrieBuilderMapNode(TrieBuilderMapNode &&) = default;
+        TrieBuilderMapNode& operator=(TrieBuilderMapNode &&) = default;
+    
+    };
+    
+    typedef std::map<char, TrieBuilderMapNode> TrieNodesMap;
+
+
+public: // пока public, потом, скорее всего, будет спрятано
+
+    TrieNodesMap     m_trieNodesMap;
+
+public:
+
+    bool addTokenSequence(const std::string &seqStr, token_id_type tokenId)
+    {
+        UMBA_ASSERT(!seqStr.empty());
+
+        TrieNodesMap *pCurMap  = &m_trieNodesMap;
+        MapTrieNode  *pCurNode = 0;
+
+        for(auto ch: seqStr)
+        {
+            TrieNodesMap 
+            &curMap  = *pCurMap;
+            pCurNode = &curMap[ch]; // .second;
+            pCurMap  = &pCurNode->childs;
+
+            pCurNode->trieNode.symbol = ch;
+
+            // ++finalTableNumEntries;
+        }
+
+        // Прошагали всё символы обрабатываемой строки
+        // Финальный узел у нас есть
+
+        if (pCurNode->trieNode.tokenId!=token_id_invalid && pCurNode->trieNode.tokenId!=tokenId)
+            return false; // Такой путь в дереве уже есть, и там другой идентификатор токена
+
+        // Записываем туда идентификатор оператора
+        pCurNode->trieNode.tokenId = token;
+
+        return true;
+    }
+
+
+
+}; // class TrieBuilder
 
 
 /*
