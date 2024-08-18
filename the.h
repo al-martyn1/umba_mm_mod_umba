3497c1c3d9e20f44782fa05735cfee19d3a94e59
Alexandr Martynov <mag@unicont.com>
2024-08-18 19:50:43 +0300

    Пилим-попилим, выпилить не можем

--- the.h
+++ the.h
@@ -1,112 +1,12 @@
 #pragma once
 
 
-// https://rsdn.org/forum/cpp.applied/8544625.1
-// https://rsdn.org/forum/cpp.applied/8555847.1
-// https://habr.com/ru/articles/248897/
-// https://habr.com/ru/articles/228031/
-// https://en.cppreference.com/w/cpp/language/parameter_pack
-// C++20 idioms for parameter packs - https://www.scs.stanford.edu/~dm/blog/param-pack.html
-
-
 #include "rule_of_five.h"
 
 
 namespace umba {
 
-
-
-template<typename TVal>
-class TheValue
-{
-
-    const TVal &tVal;
-
-
-public:
-
-    UMBA_RULE_OF_FIVE(TheValue, delete, delete, delete, delete, delete);
-
-    constexpr explicit TheValue(const TVal& v) noexcept
-      : tVal(v)
-    {
-    }
-
-    template <typename... TArgs>
-    constexpr
-    bool oneOf(TArgs&&... args) const
-    {
-        return ((tVal == args) || ...);
-    }
-
-    template <auto... VArgs>
-    constexpr
-    bool oneOf() const
-    {
-        return ((tVal == VArgs) || ...);
-    }
-
-}; // class TheValue
-
-
-
-
-
-template<typename TFlags>
-class TheFlags
-{
-
-    const TFlags &tFlags;
-
-    // template <typename... TArgs>
-    // constexpr static
-    // TFlags orImpl(TArgs&&... args)
-
-
-public:
-
-    UMBA_RULE_OF_FIVE(TheFlags, delete, delete, delete, delete, delete);
-
-    constexpr explicit TheFlags(const TFlags& f) noexcept
-      : tFlags(f)
-    {
-    }
-
-    template <typename... TArgs>
-    constexpr
-    bool oneOf(TArgs&&... args) const
-    {
-        auto flags = ((TFlags)0 | ... | args); //  | &args
-        return (tFlags & flags)!=(TFlags)0;
-    }
-
-    template <auto... VArgs>
-    constexpr
-    bool oneOf() const
-    {
-        auto flags = ((TFlags)0 | ... | VArgs);
-        return (tFlags & flags)!=(TFlags)0;
-    }
-
-    template <typename... TArgs>
-    constexpr
-    bool allOf(TArgs&&... args) const
-    {
-        auto flags = ((TFlags)0 | ... | args);
-        return (tFlags & flags)==flags;
-    }
-
-    template <auto... VArgs>
-    constexpr
-    bool allOf() const
-    {
-        auto flags = ((TFlags)0 | ... | VArgs);
-        return (tFlags & flags)==flags;
-    }
-
-}; // class TheFlags
-
-
+#include "the_.h"
 
 
 } // namespace umba
