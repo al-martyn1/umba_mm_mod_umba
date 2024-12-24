5dad43a003f889b170c742ebbed044c0adcd4897
al-martyn1 <amart@mail.ru>
2024-12-24 17:41:08 +0300

    Фикс для GCC<8

--- warnings/disable_unsafe_conversion.h
+++ warnings/disable_unsafe_conversion.h
@@ -2,7 +2,13 @@
 #include "disable_C4191.h"
 
 #if defined(__GNUC__)
-    #pragma GCC diagnostic ignored "-Wcast-function-type"
+    // https://gcc.gnu.org/onlinedocs/gcc/Warning-Options.html#index-Wcast-function-type
+    // __GNUC__              // major
+    // __GNUC_MINOR__        // minor
+    // __GNUC_PATCHLEVEL__
+    #if (__GNUC__>7) //TODO: Проверить на 8/9/10/11/12 ой версии GCC - на 13ой точно есть
+        #pragma GCC diagnostic ignored "-Wcast-function-type"
+    #endif
 #endif
 
 
