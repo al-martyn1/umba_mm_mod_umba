e0bcc54311a4aa154792f2ede0bc47e6385942b7
al-martyn1 <amart@mail.ru>
2025-04-22 19:34:17 +0300

    Добавил макросы UMBA_THE_VALUE и UMBA_THE_FLAGS, которые следует использовать вместо umba::TheValue и umba::TheFlags - более совместимо с не самыми современными компиляторами

--- the.h
+++ the.h
@@ -12,3 +12,15 @@ namespace umba {
 } // namespace umba
 
 
+#include <type_traits>
+
+
+
+// Старые компиляторы не всегда умеют выводить тип параметра шаблона
+// из фактически переданных параметров
+// Также, иногда, для базового типа это работает,
+// но если мы делаем alias при помощи шаблонного using,
+// работоспособность ломается
+
+#define UMBA_THE_VALUE(v)     umba::TheValue< std::decay_t<decltype(v)> >(v)
+#define UMBA_THE_FLAGS(v)     umba::TheFlags< std::decay_t<decltype(v)> >(v)
